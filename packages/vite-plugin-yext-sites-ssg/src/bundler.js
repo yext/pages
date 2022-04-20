b1636b840e4455d5a5857a074911fbd1058deb57
Justin Mancusi <11779614+mancusi@users.noreply.github.com>
2022-04-20 18:06:52 -0400

    update bundling and copy plugin into .yext directory (#7)
    * update bundling and copy plugin into .yext directory
    
    * add space

--- packages/vite-plugin-yext-sites-ssg/src/bundler.js
+++ packages/vite-plugin-yext-sites-ssg/src/bundler.js
@@ -1,7 +1,14 @@
 import esbuild from "esbuild";
+import { copyFile, mkdirSync, rmSync } from "fs";
 import glob from "glob";
+import path from "path";
 
 const builds = []
+
+/**
+ * rm -f dist/ to have a clean build.
+ */
+rmSync("./dist", { recursive: true, force: true });
 
 // Transpile all files except this one
 const files = glob.sync("./src/**/*\.*").filter(f => f !== "./src/bundler.js");
@@ -18,18 +25,30 @@ const commonBuildOpts = {
 
 // CJS
 try {
-  builds.push(esbuild.build({
+  builds.push(await esbuild.build({
     ...commonBuildOpts,
-    outdir: "dist/cjs",
+    outdir: "dist/cjs/src",
     format: "cjs",
   }));
 } catch (e) {console.error(e);}
 
 // ESM
 try {
-  builds.push(esbuild.build({
+  builds.push(await esbuild.build({
     ...commonBuildOpts,
-    outdir: "dist/esm",
+    outdir: "dist/esm/src",
     format: "esm",
   }));
 } catch (e) {console.error(e);}
+
+/**
+ * Copy yext plugin files from src/ to dist/ so they can be copied into the starter by the Vite
+ * plugin.
+ */
+const cjsPluginPath = "./dist/cjs/plugin/";
+const esmPluginPath = "./dist/esm/plugin/";
+const pluginFiles = glob.sync("./plugin/**.ts");
+mkdirSync(cjsPluginPath);
+mkdirSync(esmPluginPath);
+pluginFiles.map(filepath => copyFile(filepath, `${cjsPluginPath}${path.basename(filepath)}`, () => { }));
+pluginFiles.map(filepath => copyFile(filepath, `${esmPluginPath}${path.basename(filepath)}`, () => { }));
