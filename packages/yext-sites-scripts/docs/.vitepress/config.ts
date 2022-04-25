5cf3eeafb1c493890b73ddeca43d24138835863e
jimboHenris <jimboHenris@users.noreply.github.com>
2022-04-25 15:20:52 +0000

    [format-bot] apply prettier formatting changes

--- packages/yext-sites-scripts/docs/.vitepress/config.ts
+++ packages/yext-sites-scripts/docs/.vitepress/config.ts
@@ -1,43 +1,39 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 export default defineConfig({
-  lang: 'en-US',
-  title: 'Yext Sites Scripts',
-  description: 'A set of tools to allow development for Yext Sites',
+  lang: "en-US",
+  title: "Yext Sites Scripts",
+  description: "A set of tools to allow development for Yext Sites",
   lastUpdated: true,
 
   themeConfig: {
-    repo: 'yext/sites-scripts',
-    docsDir: 'docs',
-    docsBranch: 'main',
+    repo: "yext/sites-scripts",
+    docsDir: "docs",
+    docsBranch: "main",
     editLinks: false,
-    lastUpdated: 'Last Updated',
+    lastUpdated: "Last Updated",
 
-    nav: [
-      { text: 'API', link: '/api/index.html', activeMatch: '^/$|^/api/' }
-    ],
+    nav: [{ text: "API", link: "/api/index.html", activeMatch: "^/$|^/api/" }],
 
     sidebar: {
-      '/api/': getSidebar(),
-      '/': getSidebar()
-    }
-  }
-})
+      "/api/": getSidebar(),
+      "/": getSidebar(),
+    },
+  },
+});
 
 function getSidebar() {
   return [
     {
-      text: 'Introduction',
+      text: "Introduction",
       children: [
-        { text: 'What is Yext Sites Scripts?', link: '/' },
-        { text: 'API Docs', link: '/api/index.html' }
-      ]
+        { text: "What is Yext Sites Scripts?", link: "/" },
+        { text: "API Docs", link: "/api/index.html" },
+      ],
     },
     {
-      text: 'Advanced',
-      children: [
-        { text: 'TODO - add stuff', link: '/' }
-      ]
-    }
-  ]
+      text: "Advanced",
+      children: [{ text: "TODO - add stuff", link: "/" }],
+    },
+  ];
 }
