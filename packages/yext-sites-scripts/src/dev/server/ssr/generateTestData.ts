be83d0aa9625713a438c0c9a1028b23e944420e6
Jim Henris <jhenris@yext.com>
2022-04-26 11:22:40 -0400

    WIP: Add unit tests
    WIP of my work so far to add a unit test for generateTestData,
    running into weird issues with Jest and TypeScript.
    
    J=SUMO-4393
    TEST=none
    
    Still WIP.

--- packages/yext-sites-scripts/src/dev/server/ssr/generateTestData.ts
+++ packages/yext-sites-scripts/src/dev/server/ssr/generateTestData.ts
@@ -1,12 +1,19 @@
 import { spawn } from "child_process";
+import {
+  CLI_BOILERPLATE_CHUNK_BEGIN,
+  STREAM_DATA_CHUNK_BEGIN,
+} from "./constants";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const generateTestData = async (
   featureConfig: any,
   entityId: string
-): Promise<any> => {
+): Promise<string> => {
   return new Promise((resolve) => {
-    const generateTestDataExec = spawn(
+    let testData = "";
+
+    // spawn a child process that will call generate-test-data via the
+    // CLI for the relevant entity and print the output directly to stdout.
+    const generateTestDataChildProcess = spawn(
       "yext",
       [
         "sites",
@@ -27,36 +34,46 @@ export const generateTestData = async (
       }
     );
 
-    let testData = "";
+    // Read the child process' stdout and stderr streams as UTF-8 encoded strings.
+    generateTestDataChildProcess.stdout.setEncoding("utf8");
+    generateTestDataChildProcess.stderr.setEncoding("utf8");
 
-    generateTestDataExec.stdout.setEncoding("utf8");
-    generateTestDataExec.stdout.on("data", (chunk) => {
-      if (chunk.startsWith("{")) {
+    // Pipe input from the parent process to the generateTestDataChildProcess so the
+    // user can respond to any interactive prompts (i.e. auth flow) from the parent
+    // process.
+    process.stdin.pipe(generateTestDataChildProcess.stdin);
+
+    // When the child process receives a chunk of data on stdout, redirect it to the
+    // appropriate place.
+    generateTestDataChildProcess.stdout.on("data", (chunk) => {
+      // If the chunk is actual stream data, write to local variable.
+      if (chunk.startsWith(STREAM_DATA_CHUNK_BEGIN)) {
         testData += chunk;
-      } else if (
-        chunk.includes(
-          "This is a beta version of the Yext Command Line Interface"
-        )
-      ) {
+      }
+      // If the chunk is CLI Boilerplate, ignore.
+      else if (chunk.startsWith(CLI_BOILERPLATE_CHUNK_BEGIN)) {
         return;
-      } else {
+      }
+      // This case will act as a catch-all to write back any prompts to the user to
+      // the parent process. It's main usage is to allow the user to go through the
+      // authentication flow from the parent process.
+      else {
         process.stdout.write(chunk);
       }
     });
 
-    generateTestDataExec.stderr.setEncoding("utf8");
-    generateTestDataExec.stderr.on("data", (chunk) => {
+    // Write back anything on stderr to the parent process so the user can see and respond.
+    generateTestDataChildProcess.stderr.on("data", (chunk) => {
       process.stderr.write(chunk);
     });
 
-    generateTestDataExec.on("close", () => {
+    // When the stream is closed, resolve with the testData we've collected.
+    generateTestDataChildProcess.on("close", () => {
       if (testData) {
         testData = JSON.parse(testData.trim());
       }
 
       resolve(testData);
     });
-
-    process.stdin.pipe(generateTestDataExec.stdin);
   });
 };
--- packages/yext-sites-scripts/tests/dev/ssr/generateTestData.test.ts
+++ packages/yext-sites-scripts/tests/dev/ssr/generateTestData.test.ts
@@ -1,5 +1,36 @@
-describe("I can run a simple test", () => {
-  it("properly runs my simple test", () => {
+import "jest";
+import {
+  SpawnOptionsWithoutStdio,
+  ChildProcessWithoutNullStreams,
+} from "child_process";
+
+jest.mock("child_process", () => ({
+  ...(jest.requireActual("child_process") as object),
+  spawn: jest.fn(
+    (
+      command: string,
+      args?: readonly string[] | undefined,
+      options?: SpawnOptionsWithoutStdio | undefined
+    ): ChildProcessWithoutNullStreams => {
+      return {};
+    }
+  ),
+}));
+
+describe("generateTestData", () => {
+  it("properly spawns a child process", async () => {
+    expect(1).toBeTruthy();
+  });
+
+  it("properly redirects the stdout of the child process", async () => {
+    expect(1).toBeTruthy();
+  });
+
+  it("properly redirects the stderr of the child process", async () => {
+    expect(1).toBeTruthy();
+  });
+
+  it("propoerly pipes input from parent to child process", async () => {
     expect(1).toBeTruthy();
   });
 });
