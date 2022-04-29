12cddae9e34126b06d48757c49a7a17df4d702a4
Jim Henris <jhenris@yext.com>
2022-04-29 11:16:11 -0400

    Only show the upgrade messages from CLI Boilerplate

--- packages/yext-sites-scripts/tests/fixtures/cli_boilerplate.ts
+++ packages/yext-sites-scripts/tests/fixtures/cli_boilerplate.ts
@@ -1,4 +1,8 @@
-export const CLI_BOILERPLATE = `This is a beta version of the Yext Command Line Interface
+export const CLI_BOILERPLATE_WITHOUT_UPGRADE_LINES = `This is a beta version of the Yext Command Line Interface
+
+`;
+
+export const CLI_BOILERPLATE_WITH_UPGRADE_LINES = `This is a beta version of the Yext Command Line Interface
 
 Please upgrade the Yext CLI. Current version: 0.1_248. Top version: 0.1_250.
 
@@ -6,3 +10,6 @@ In order to upgrade, please run \`brew upgrade yext\`
 
 
 `;
+
+export const UPGRADE_LINES_OF_CLI_BOILERPLATE = `Please upgrade the Yext CLI. Current version: 0.1_248. Top version: 0.1_250.
+In order to upgrade, please run \`brew upgrade yext\``;
