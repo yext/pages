b15b8756acd94719a0bafcb0ea166bfd1eeb6111
Duval Kilpatrick <mkilpatrick@yext.com>
2022-05-25 18:29:52 -0400

    Add common package with files/types dealing with TemplateModules and cog features.json

---
+++ packages/common/feature/stream.ts
@@ -0,0 +1,26 @@
+import { Config } from "../templateModule/types";
+
+export type StreamConfig = {
+    $id: string;
+    source: string;
+    destination: string;
+    fields: string[];
+    filter: {
+        entityTypes?: string[];
+        savedFilterIds?: string[];
+    };
+    localization: {
+        locales: string[];
+        primary: boolean;
+    };
+} 
+
+export const convertConfigToValidStreamFormat = (config: Config): (StreamConfig | void) => {
+    if (config.stream) {
+        return {
+            ...config.stream,
+            source: "knowledgeGraph",
+            destination: "pages",
+        }
+    }
+}
