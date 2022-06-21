9bcbf52ad2d37ac5c719847657301c2255e33917
Justin Mancusi <jmancusi@yext.com>
2022-06-21 17:26:23 -0400

    update parsing logic

--- packages/yext-sites-scripts/src/dev/server/ssr/constants.ts
+++ packages/yext-sites-scripts/src/dev/server/ssr/constants.ts
@@ -1,6 +1,7 @@
 export const TEMPLATE_PATH = "src/templates";
 export const STREAM_DATA_CHUNK_BEGIN = "{";
-export const CLI_BOILERPLATE_CHUNK_BEGIN =
+export const STREAM_DATA_CHUNK_END = "}";
+export const CLI_BOILERPLATE_BETA_MESSAGE =
   "This is a beta version of the Yext Command Line Interface";
 export const UPGRADE_MESSAGE_LINE_BEGIN = "Please upgrade the Yext CLI";
 export const UPGRADE_INSTRUCTIONS_LINE_BEGIN = "In order to upgrade";
--- packages/yext-sites-scripts/src/dev/server/ssr/generateTestData.ts
+++ packages/yext-sites-scripts/src/dev/server/ssr/generateTestData.ts
@@ -1,7 +1,7 @@
 import { spawn } from "child_process";
 import { FeaturesConfig } from "../../../../../common/src/feature/features";
 import {
-  CLI_BOILERPLATE_CHUNK_BEGIN,
+  CLI_BOILERPLATE_BETA_MESSAGE,
   STREAM_DATA_CHUNK_BEGIN,
   UPGRADE_MESSAGE_LINE_BEGIN,
   UPGRADE_INSTRUCTIONS_LINE_BEGIN,
@@ -12,69 +12,82 @@ export const generateTestData = async (
   featuresConfig: FeaturesConfig,
   entityId: string
 ): Promise<any> => {
+  const command = "yext";
+  const args = [
+    "sites",
+    "generate-test-data",
+    "--featureName",
+    `'${featuresConfig.features[0]?.name}'`,
+    "--entityId",
+    entityId,
+    "--featuresConfig",
+    `'${JSON.stringify(featuresConfig)}'`,
+    "--locale",
+    "en",
+    "--printDocuments",
+  ];
   return new Promise((resolve) => {
-    let testData = "";
-    const childProcess = spawn(
-      "yext",
-      [
-        "sites",
-        "generate-test-data",
-        "--featureName",
-        `'${featuresConfig.features[0]?.name}'`,
-        "--entityId",
-        entityId,
-        "--featuresConfig",
-        `'${JSON.stringify(featuresConfig)}'`,
-        "--locale",
-        "en",
-        "--printDocuments",
-      ],
-      {
-        stdio: ["inherit", "pipe", "inherit"],
-        shell: true,
-      }
-    );
+    const childProcess = spawn(command, args, {
+      stdio: ["inherit", "pipe", "inherit"],
+      shell: true,
+    });
 
     // Assume that all CLI chunks will come before any stream data. Once stream data is found
     // assume the rest is relevant.
+    let testData = "";
     let foundTestData = false;
     childProcess.stdout.on("data", (chunkBuff: Buffer) => {
       const chunk = chunkBuff.toString("utf-8");
+      // Remove the Yext boilerplate
+      let lines = chunk
+        .split("\n")
+        .filter((l) => !l.startsWith(CLI_BOILERPLATE_BETA_MESSAGE));
 
-      // If the chunk is actual stream data, write to local variable.
-      if (chunk.startsWith(STREAM_DATA_CHUNK_BEGIN) || foundTestData) {
-        foundTestData = true;
+      // If we've found test data at all then we assume the rest of the output is test data.
+      if (foundTestData) {
         testData += chunk;
         return;
+      }
+
+      // Check to see if the test data has begun to be printed in this chunk.
+      const dataStartIndex = lines.indexOf(STREAM_DATA_CHUNK_BEGIN);
+      if (dataStartIndex !== -1) {
+        foundTestData = true;
+        testData = lines.slice(dataStartIndex).join("\n");
+        lines = lines.slice(0, dataStartIndex);
       }
 
       // If the CLI Boilerplate indicates that the user's version of the YextCLI is
       // out of date, write back the relevant lines to the parent process so they can
       // see their current version relative to the most recent and upgrade instructions.
-      if (chunk.startsWith(CLI_BOILERPLATE_CHUNK_BEGIN)) {
-        const upgadeLinesInCliBoilerplate = chunk
-          .split("\n")
-          .filter(
-            (boilerplateLine) =>
-              boilerplateLine.startsWith(UPGRADE_MESSAGE_LINE_BEGIN) ||
-              boilerplateLine.startsWith(UPGRADE_INSTRUCTIONS_LINE_BEGIN)
-          )
-          .join("\n");
+      const upgradeLines = lines
+        .filter(
+          (boilerplateLine) =>
+            boilerplateLine.startsWith(UPGRADE_MESSAGE_LINE_BEGIN) ||
+            boilerplateLine.startsWith(UPGRADE_INSTRUCTIONS_LINE_BEGIN)
+        )
+        .join("\n");
 
-        upgadeLinesInCliBoilerplate &&
-          stdout.write(upgadeLinesInCliBoilerplate);
-        return;
+      if (upgradeLines) {
+        stdout.write(upgradeLines);
+      } else {
+        // This will act as a catch-all to write back any prompts to the the parent process
+        // so the user can see it. Its main usage is to allow the user to go through the
+        // authentication flow from the parent process.
+        const out = lines.join("\n").trim();
+        out && stdout.write(out)
       }
-
-      // This will act as a catch-all to write back any prompts to the the parent process
-      // so the user can see it. Its main usage is to allow the user to go through the
-      // authentication flow from the parent process.
-      stdout.write(chunk);
     });
 
     childProcess.on("close", () => {
       if (testData) {
         testData = JSON.parse(testData.trim());
+      } else {
+        stdout.write(
+          `Unable to generate test data from command: \`${command}${args.join(
+            " "
+          )}\``
+        );
       }
 
       resolve(testData);
