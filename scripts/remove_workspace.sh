336ba67b2180db51ee0523021fc0cc14991d92a6
jimboHenris <69811977+jimboHenris@users.noreply.github.com>
2022-07-01 10:01:47 -0400

    Update github action to automatically generate licenses (#78)
    * Update github action to automatically generate licenses
    
    * Update generate_licenses
    
    * Do everything in the one yaml file
    
    * Have to use npm i instead of ci because there isn't a package-lock present
    
    * Add echo statements to help debug
    
    * Add more printing
    
    * Fix add command
    
    * Automated update to THIRD-PARTY-NOTICES from github action's 3rd party notices check
    
    * Generate root level third party licenses
    
    * Remove the :local option since it doesn't work with workspaces
    anyway
    
    * Fix syntax error
    
    * Remove all third party notices as a test
    
    * Automated update to THIRD-PARTY-NOTICES from github action's 3rd party notices check
    
    * remove debug prints
    
    * Automated update to THIRD-PARTY-NOTICES from github action's 3rd party notices check
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ scripts/remove_workspace.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This sed command will remove everything between the "workspaces"
+# key and the next ']' encountered. It will only work with the
+# GNU version of sed.
+sed -i '/\"workspaces\"/,/]/ d; /^$/d' package.json
