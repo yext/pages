f0940332b9d8cc44db81f18c3235b2b0949e8402
jimboHenris <69811977+jimboHenris@users.noreply.github.com>
2022-07-08 14:16:04 -0400

    Fix annoying red squigglies (#86)

--- packages/common/src/project/structure.ts
+++ packages/common/src/project/structure.ts
@@ -1,5 +1,5 @@
-import path from "path";
-import _ from "lodash";
+import * as path from "path";
+import * as _ from "lodash";
 
 /**
  * Defines the folder paths where certain files live, relative to the root of the project.
