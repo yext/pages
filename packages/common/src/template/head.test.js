0c0fd10992fd66004c617a7dbacb0bba544f9cff
Anh Thu Nguyen <anguyen@yext.com>
2022-07-14 12:05:19 -0400

    addressed Duval's comment -- mainly added a test that console logs the warning when tag type is not supported

--- packages/common/src/template/head.test.js
+++ packages/common/src/template/head.test.js
@@ -1,4 +1,5 @@
 import { renderHeadConfigToString } from "./head";
+import pc from "picocolors";
 
 let consoleLog;
 beforeAll(() => {
@@ -32,5 +33,35 @@ describe("renderHeadConfigToString", () => {
     expect(renderHeadConfigToString(headConfig).replaceAll(" ", "")).toEqual(
       expectedHeadConfig.replaceAll(" ", "")
     );
+  });
+  it("properly logs warning when rendering tag whose type is not supported", async () => {
+    const headConfig = {
+      title: "foo",
+      charset: "UTF-8",
+      viewport: "bar",
+      tags: [
+        {
+          type: "wrongtype",
+          attributes: {
+            details: "shouldnotrender",
+          },
+        },
+      ],
+    };
+
+    const expectedLog = pc.yellow(
+      `[WARNING]: Tag type wrongtype is unsupported by the Tag interface. ` +
+        `Please use "other" to render this tag.`
+    );
+
+    jest.clearAllMocks();
+    jest.spyOn(console, 'log');
+
+    expect(console.log.mock.calls.length).toBe(0);
+    renderHeadConfigToString(headConfig);
+    expect(console.log.mock.calls.length).toBe(1);
+    expect(console.log.mock.calls[0][0]).toBe(expectedLog);
+    
+    jest.clearAllMocks();
   });
 });
