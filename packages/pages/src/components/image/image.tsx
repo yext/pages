76c416af01a143be9d1d49fb2d93e71b6a04fc0b
Emily Zhang <ezhang@yext.com>
2022-08-01 17:18:32 -0400

    address comments

--- packages/pages/src/components/image/image.tsx
+++ packages/pages/src/components/image/image.tsx
@@ -1,14 +1,34 @@
 import * as React from "react";
-import { useRef, useState, useEffect } from "react";
-import { ImageProps } from "./types";
+import {useEffect, useRef, useState} from "react";
+import {ImageProps, ImageLayout} from "./types";
 
+/**
+ * A component that renders an image. Here is an example of using the component with simple and
+ * complex image fields from Knowledge Graph:
+ * ```
+ * import { Image } from "@yext/pages/components";
+ *
+ * const template = ({ document }) => {
+ *   return (
+ *     <div>
+ *       <Image image={document.logo.image} />
+ *       <Image image={document.photoGallery[0].image} />
+ *     </div>
+ *   );
+ * }
+ * ```
+ *
+ * `layout`, `imgOverrides` and `style` can be used to specify the style and behavior of the image.
+ *
+ * @public
+ */
 export const Image = ({
   image,
   className,
   width,
   height,
   aspectRatio,
-  layout = "intrinsic",
+  layout = ImageLayout.INTRINSIC,
   placeholder,
   imgOverrides,
   style = {},
@@ -22,87 +42,21 @@ export const Image = ({
     }
   }, []);
 
-  if (layout != "fixed" && (width || height)) {
+  if (layout != ImageLayout.FIXED && (width || height)) {
     console.warn(
       "Width or height is passed in but layout is not fixed. These will have no impact. If you want to have a fixed height or width then set layout to fixed."
     );
   }
 
-  const imgWidth: number = image.image.width;
-  const imgHeight: number = image.image.height;
-  const imgUUID: string = image.image.url.split("/")[4];
-
-  style.objectFit = style.objectFit || "cover";
-  style.objectPosition = style.objectPosition || "center";
-
-  let widths: number[] = [100, 320, 640, 960, 1280, 1920];
-  let src: string = getImageUrl(imgUUID, 500, 500);
-
-  switch (layout) {
-    case "intrinsic":
-      // Don't let image be wider than its intrinsic width
-      style.maxWidth = imgWidth;
-      style.width = "100%";
-      style.aspectRatio = aspectRatio
-        ? `${aspectRatio}`
-        : `${imgWidth} / ${imgHeight}`;
-
-      break;
-    case "fixed":
-      if (!width && !height) {
-        console.warn(
-          "Using fixed layout but width and height are not passed as props."
-        );
-      }
-
-      const fixedWidth = width
-        ? width
-        : height
-        ? (height / imgHeight) * imgWidth
-        : undefined;
-      const fixedHeight = height
-        ? height
-        : width
-        ? (width * imgHeight) / imgWidth
-        : undefined;
-
-      style.width = fixedWidth;
-      style.height = fixedHeight;
-
-      if (fixedWidth && fixedHeight) {
-        src = getImageUrl(imgUUID, fixedWidth, fixedHeight);
-      }
-
-      widths = width
-        ? [width]
-        : height
-        ? [(height / imgHeight) * imgWidth]
-        : widths;
-
-      break;
-    case "aspect":
-      if (!aspectRatio) {
-        console.warn(
-          "Using aspect layout but aspectRatio is not passed as a prop."
-        );
-      }
-
-      style.aspectRatio = aspectRatio
-        ? `${aspectRatio}`
-        : `${imgWidth} / ${imgHeight}`;
-
-      break;
-    case "fill":
-      style.width = "100%";
-      style.aspectRatio = aspectRatio
-        ? `${aspectRatio}`
-        : `${imgWidth} / ${imgHeight}`;
-
-      break;
-    default:
-      console.warn(`Unrecognized layout: ${layout}.`);
-      break;
+  const imgWidth: number = image.width;
+  const imgHeight: number = image.height;
+  const imgUrlValid = !!image.url && image.url.split("/").length > 4;
+  if (!imgUrlValid) {
+    console.warn(`Invalid image url: ${image.url}.`);
   }
+  const imgUUID: string = imgUrlValid ? image.url.split("/")[4] : "";
+
+  const {src, imgStyle, widths} = handleLayout(layout, imgWidth, imgHeight, imgUUID, style, width, height, aspectRatio);
 
   // Generate Image Sourceset
   const srcSet: string = widths
@@ -114,7 +68,7 @@ export const Image = ({
       {placeholder != null && !imgLoaded && placeholder}
       <img
         ref={imgRef}
-        style={style}
+        style={imgStyle}
         src={src}
         className={className}
         width={width}
@@ -133,3 +87,86 @@ const getImageUrl = (uuid: string, width: number, height: number) => {
     height
   )}`;
 };
+
+const handleLayout = (
+    layout: ImageLayout,
+    imgWidth: number,
+    imgHeight: number,
+    imgUUID: string,
+    style: React.CSSProperties,
+    width?: number,
+    height?: number,
+    aspectRatio?: number,
+): { src: string; imgStyle: React.CSSProperties; widths: number[] } => {
+  let widths: number[] = [100, 320, 640, 960, 1280, 1920];
+  let src: string = getImageUrl(imgUUID, 500, 500);
+  style.objectFit = style.objectFit || "cover";
+  style.objectPosition = style.objectPosition || "center";
+
+  switch (layout) {
+    case ImageLayout.INTRINSIC:
+      // Don't let image be wider than its intrinsic width
+      style.maxWidth = imgWidth;
+      style.width = "100%";
+      style.aspectRatio = aspectRatio
+          ? `${aspectRatio}`
+          : `${imgWidth} / ${imgHeight}`;
+
+      break;
+    case ImageLayout.FIXED:
+      if (!width && !height) {
+        console.warn(
+            "Using fixed layout but width and height are not passed as props."
+        );
+      }
+
+      const fixedWidth = width
+          ? width
+          : height
+              ? (height / imgHeight) * imgWidth
+              : undefined;
+      const fixedHeight = height
+          ? height
+          : width
+              ? (width * imgHeight) / imgWidth
+              : undefined;
+
+      style.width = fixedWidth;
+      style.height = fixedHeight;
+
+      if (fixedWidth && fixedHeight) {
+        src = getImageUrl(imgUUID, fixedWidth, fixedHeight);
+      }
+
+      widths = width
+          ? [width]
+          : height
+              ? [(height / imgHeight) * imgWidth]
+              : widths;
+      break;
+    case ImageLayout.ASPECT:
+      if (!aspectRatio) {
+        console.warn(
+            "Using aspect layout but aspectRatio is not passed as a prop."
+        );
+      }
+
+      style.aspectRatio = aspectRatio
+          ? `${aspectRatio}`
+          : `${imgWidth} / ${imgHeight}`;
+
+      break;
+    case ImageLayout.FILL:
+      style.width = "100%";
+      style.aspectRatio = aspectRatio
+          ? `${aspectRatio}`
+          : `${imgWidth} / ${imgHeight}`;
+
+      break;
+    default:
+      console.warn(`Unrecognized layout: ${layout}.`);
+      break;
+  }
+
+  return {src, imgStyle: style, widths};
+}
--- packages/pages/src/components/image/types.ts
+++ packages/pages/src/components/image/types.ts
@@ -7,22 +7,52 @@ export type ThumbnailType = {
 };
 
 export type ImageType = {
-  image: {
-    height: number;
-    width: number;
-    url: string;
-    thumbnails?: ThumbnailType[];
-  };
+  height: number;
+  width: number;
+  url: string;
+  thumbnails?: ThumbnailType[];
 };
 
+export enum ImageLayout {
+  /**
+   * The the default layout if one is not specified. An image will be scaled down to fit the
+   * container but not exceed the absolute size of the image.
+   */
+  INTRINSIC = "intrinsic",
+  /**
+   * Shows the image in a fixed size. `width` or `height` must be passed in. If both "width` and
+   * `height` are passed in, but the aspect ratio does not match the aspect ratio of the image,
+   * the image will be centered. This behavior can be adjusted using the `objectFit` and
+   * `objectPosition` props of the `style` rpop.
+   */
+  FIXED = "fixed",
+  /** Shows the image in a fixed aspect ratio. The `aspectRatio` prop must be provided. */
+  ASPECT = "aspect",
+  /** Always fills the image to 100% of the container's width. */
+  FILL = "fill",
+}
+
+/**
+ * The shape of the data passed directly to the different template functions with the
+ * exception of the render function (getPath, getHeadConfig, etc).
+ */
 export type ImageProps = {
+  /** The image field from Knowledge Graph. */
   image: ImageType;
+  /** Overrides the className on the underlying img tag. */
   className?: string;
+  /** The absolute width of the image. Only impacts if layout is set to "fixed". */
   width?: number;
+  /** The absolute height of the image. Only impacts if layout is set to "fixed". */
   height?: number;
+  /** The aspect ratio of the image. Only impacts if layout is set to "aspect". */
   aspectRatio?: number;
-  layout?: "intrinsic" | "fixed" | "aspect" | "fill";
+  /** Specifies how the image is rendered. */
+  layout?: ImageLayout;
+  /** A pass through react component that is displayed when the image is loading. */
   placeholder?: React.ReactNode;
+  /** Pass through props that are on the native HTML img tag. The Image component may not work if src and/or srcsets are included. */
   imgOverrides?: Object;
+  /** The pass through style of the underlying img tag. */
   style?: React.CSSProperties;
 };
