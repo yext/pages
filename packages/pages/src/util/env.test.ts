148f4d8a2769f1c568a8ee04f2444909f72a119a
Duval Kilpatrick <mkilpatrick@yext.com>
2022-08-05 01:07:48 -0400

    feat(util): add isProduction function

---
+++ packages/pages/src/util/env.test.ts
@@ -0,0 +1,88 @@
+/**
+ * @jest-environment jsdom
+ */
+import { isProduction } from "./env";
+import * as runTime from "./runtime";
+
+describe("isProduction", () => {
+  it("returns true when browser and prod domain", async () => {
+    const runtimeSpy = jest.spyOn(runTime, "getRuntime") as jest.MockInstance<
+      any,
+      any
+    >;
+    runtimeSpy.mockImplementation(() => ({
+      name: "browser",
+    }));
+
+    const domain = "prod.com";
+
+    const windowSpy = jest.spyOn(window, "window", "get") as jest.MockInstance<
+      any,
+      any
+    >;
+    windowSpy.mockImplementation(() => ({
+      location: {
+        hostname: "prod.com",
+      },
+    }));
+
+    expect(isProduction(domain)).toBeTruthy();
+
+    windowSpy.mockRestore();
+    runtimeSpy.mockRestore();
+  });
+
+  it("returns false when not browser and prod domain", async () => {
+    const runtimeSpy = jest.spyOn(runTime, "getRuntime") as jest.MockInstance<
+      any,
+      any
+    >;
+    runtimeSpy.mockImplementation(() => ({
+      name: "node",
+    }));
+
+    const domain = "prod.com";
+
+    const windowSpy = jest.spyOn(window, "window", "get") as jest.MockInstance<
+      any,
+      any
+    >;
+    windowSpy.mockImplementation(() => ({
+      location: {
+        hostname: "prod.com",
+      },
+    }));
+
+    expect(isProduction(domain)).toBeFalsy();
+
+    windowSpy.mockRestore();
+    runtimeSpy.mockRestore();
+  });
+
+  it("returns false when browser and staging domain", async () => {
+    const runtimeSpy = jest.spyOn(runTime, "getRuntime") as jest.MockInstance<
+      any,
+      any
+    >;
+    runtimeSpy.mockImplementation(() => ({
+      name: "browser",
+    }));
+
+    const domain = "prod.com";
+
+    const windowSpy = jest.spyOn(window, "window", "get") as jest.MockInstance<
+      any,
+      any
+    >;
+    windowSpy.mockImplementation(() => ({
+      location: {
+        hostname: "prod.com",
+      },
+    }));
+
+    expect(isProduction("staging.com")).toBeFalsy();
+
+    windowSpy.mockRestore();
+    runtimeSpy.mockRestore();
+  });
+});
