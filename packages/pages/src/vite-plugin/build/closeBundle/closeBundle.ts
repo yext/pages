6a16d2a7efe3a4f91f096e2b88bcbbddbc53b324
Matt Kilpatrick <mkilpatrick@yext.com>
2022-08-05 16:53:02 -0400

    feat(plugin): validate file sizes (closes #145) (#147)
    * feat(plugin): validate file sizes (fixes 145)
    
    * [format-bot] apply prettier formatting changes
    
    * validate all plugin files
    
    * add tests
    
    * address comments
    
    Co-authored-by: mkilpatrick <mkilpatrick@users.noreply.github.com>

--- packages/pages/src/vite-plugin/build/closeBundle/closeBundle.ts
+++ packages/pages/src/vite-plugin/build/closeBundle/closeBundle.ts
@@ -8,6 +8,8 @@ import {
   TemplateModuleCollection,
 } from "./moduleLoader.js";
 import { ProjectStructure } from "../../../common/src/project/structure.js";
+import colors from "picocolors";
+import { validateBundles } from "./bundleValidator.js";
 
 export default (projectStructure: ProjectStructure) => {
   return async () => {
@@ -21,10 +23,11 @@ export default (projectStructure: ProjectStructure) => {
         )
       );
       templateModules = await loadTemplateModules(serverBundles);
+      validateBundles();
       finisher.succeed("Validated template modules");
-    } catch (e) {
+    } catch (e: any) {
       finisher.fail("One or more template modules failed validation");
-      console.error(e);
+      console.error(colors.red(e.message));
       return;
     }
 
@@ -37,9 +40,9 @@ export default (projectStructure: ProjectStructure) => {
         path.join(sitesConfigRoot, projectStructure.featuresConfig)
       );
       finisher.succeed(`Successfully wrote ${sitesConfigRoot}`);
-    } catch (e) {
+    } catch (e: any) {
       finisher.fail(`Failed to write ${sitesConfigRoot}`);
-      console.error(e);
+      console.error(colors.red(e.message));
       return;
     }
 
@@ -47,9 +50,9 @@ export default (projectStructure: ProjectStructure) => {
     try {
       await generateManifestFile(featureNameToBundlePath, projectStructure);
       finisher.succeed("Successfully wrote manifest.json");
-    } catch (e) {
+    } catch (e: any) {
       finisher.fail("Failed to write manifest.json");
-      console.error(e);
+      console.error(colors.red(e.message));
     }
   };
 };
