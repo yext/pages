97a9c56de441e134440ba8f391e7927694f5cf70
bhaines <bhaines@yext.com>
2022-09-08 14:43:37 -0400

    move plugins folder to new workspace

--- packages/plugins/urlWriteback/index.ts
+++ packages/plugins/urlWriteback/index.ts
@@ -1,20 +1,33 @@
 import { updateEntity } from "./yext.ts";
 
-interface UrlWritebackConfig {
+/**
+ * The shape of the configuration options for the writeback plugin
+ */
+export interface UrlWritebackConfig {
   field: string
   apiKey: string
   environment?: "sbx" | "prod"
   v?: string
 }
 
-export interface WritebackPayload {
+/**
+ * The shape of the event triggered by the publishing system
+ */
+interface WritebackPayload {
   url: string;
   entityId: string;
   feature: string;
   locale: string;
 }
 
-export default function urlWritebackPlugin(config: UrlWritebackConfig) {
+/**
+ * The urlWriteback function returns a configured function that can be used
+ * as a handler for the publishing system onUrlChange event in order to 
+ * save published urls back into the knowledge graph
+ *
+ * @public
+ */
+export default function urlWriteback(config: UrlWritebackConfig) {
   function onUrlChange(event: WritebackPayload) {
     if (!(event.entityId && event.locale && event.url)) { return null }
 
