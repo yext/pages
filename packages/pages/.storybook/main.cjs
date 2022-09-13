42087dfb0065dcd21f497c5446a4d79a8b80556f
Tristan Timblin <ttimblin@yext.com>
2022-09-13 10:00:19 -0400

    chore(components): add addons

--- packages/pages/.storybook/main.cjs
+++ packages/pages/.storybook/main.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-vite",
