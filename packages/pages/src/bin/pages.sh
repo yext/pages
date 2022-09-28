eeb625d33dddf7135c2f3eadfe93f3e7a9811653
Duval Kilpatrick <mkilpatrick@yext.com>
2022-09-27 21:43:28 -0400

    fix: features command when using top-level await

--- packages/pages/src/bin/pages.sh
+++ packages/pages/src/bin/pages.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/bin/env node --experimental-specifier-resolution=node node_modules/@yext/pages/dist/bin/pages.js "$@"
+/usr/bin/env node --experimental-specifier-resolution=node --experimental-vm-modules node_modules/@yext/pages/dist/bin/pages.js "$@"
