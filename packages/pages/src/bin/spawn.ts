24a80b54ab2d3126fba8f8a28cfc9d5b6391387e
Oliver Shi <oshi@yext.com>
2022-12-07 15:44:09 -0500

    see what happens if I remove flags

--- packages/pages/src/bin/spawn.ts
+++ packages/pages/src/bin/spawn.ts
@@ -4,8 +4,8 @@ import { spawnSync } from "child_process";
 spawnSync(
   "node",
   [
-    "--experimental-specifier-resolution=node",
-    "--experimental-vm-modules",
+    // "--experimental-specifier-resolution=node",
+    // "--experimental-vm-modules",
     "node_modules/@yext/pages/dist/bin/pages.js",
     ...process.argv.slice(2),
   ],
