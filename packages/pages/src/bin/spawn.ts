9f6949d91156a15eaceb91c7752169b4ce5f3667
Oliver Shi <oshi@yext.com>
2022-12-07 18:24:18 -0500

    do i need to use path.resolve for windows

--- packages/pages/src/bin/spawn.ts
+++ packages/pages/src/bin/spawn.ts
@@ -1,14 +1,27 @@
 #!/usr/bin/env node
 import { spawnSync } from "child_process";
+import process from "process";
+import path from "path";
 
-console.log("spawning");
+console.log("spawning!");
+
+const pathToPagesScript = path.resolve(
+  process.cwd(),
+  "node_modules",
+  "@yext",
+  "pages",
+  "dist",
+  "bin",
+  "pages.js"
+);
+console.log(process.cwd(), pathToPagesScript);
 
 spawnSync(
   "node",
   [
     "--experimental-specifier-resolution=node",
     "--experimental-vm-modules",
-    "node_modules/@yext/pages/dist/bin/pages.js",
+    pathToPagesScript,
     ...process.argv.slice(2),
   ],
   {
