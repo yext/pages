3f4abccfbb6ddae8528449928f5225587495d736
Oliver Shi <oshi@yext.com>
2022-12-07 18:54:23 -0500

    make pages.js just log stuff

--- packages/pages/src/bin/pages.ts
+++ packages/pages/src/bin/pages.ts
@@ -1,34 +1,38 @@
 #!/usr/bin/env node
-import { initCommandModule } from "../init/init.js";
-import { devCommandModule } from "../dev/dev.js";
-import { generateCommandModule } from "../generate/generate.js";
-import { buildCommandModule } from "../build/build.js";
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
+import path from "path";
 
-console.log("start pages.js", process.argv);
+console.log("pages.boop");
 
-// pages requires react and react-dom be installed to function appropriately. If
-// these are not installed in instruct the user to install them.
-["react", "react-dom"].forEach((dep) => {
-  try {
-    import(dep);
-  } catch (e) {
-    console.error(
-      `Cannot find "${dep}" which is a necessary dependency for generation. Please install this module.`
-    );
-    process.exit(1);
-  }
-});
+// import { initCommandModule } from "../init/init.js";
+// import { devCommandModule } from "../dev/dev.js";
+// import { generateCommandModule } from "../generate/generate.js";
+// import { buildCommandModule } from "../build/build.js";
+// import yargs from "yargs";
+// import { hideBin } from "yargs/helpers";
 
-yargs(hideBin(process.argv))
-  .scriptName("pages")
-  .command(devCommandModule)
-  .command(initCommandModule)
-  .command(generateCommandModule)
-  .command(buildCommandModule)
-  .demandCommand()
-  .version(false)
-  .strict()
-  .help()
-  .parse();
+// console.log("start pages.js", process.argv);
+
+// // pages requires react and react-dom be installed to function appropriately. If
+// // these are not installed in instruct the user to install them.
+// ["react", "react-dom"].forEach((dep) => {
+//   try {
+//     import(dep);
+//   } catch (e) {
+//     console.error(
+//       `Cannot find "${dep}" which is a necessary dependency for generation. Please install this module.`
+//     );
+//     process.exit(1);
+//   }
+// });
+
+// yargs(hideBin(process.argv))
+//   .scriptName("pages")
+//   .command(devCommandModule)
+//   .command(initCommandModule)
+//   .command(generateCommandModule)
+//   .command(buildCommandModule)
+//   .demandCommand()
+//   .version(false)
+//   .strict()
+//   .help()
+//   .parse();
