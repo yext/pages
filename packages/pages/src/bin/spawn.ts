76e8ba98973aa2ee89bcfb24a8bd54a9cb166e3a
Oliver Shi <oshi@yext.com>
2022-12-07 15:31:59 -0500

    spawn and inherit

--- packages/pages/src/bin/spawn.ts
+++ packages/pages/src/bin/spawn.ts
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import { spawnSync } from "child_process";
 
-console.log("spawny");
 spawnSync(
-  "node" +
-    " --experimental-specifier-resolution=node" +
-    " --experimental-vm-modules" +
-    " node_modules/@yext/pages/dist/bin/pages.js" +
-    ' "$@"'
+  "node",
+  [
+    "--experimental-specifier-resolution=node",
+    "--experimental-vm-modules",
+    "node_modules/@yext/pages/dist/bin/pages.js",
+    ...process.argv.slice(2),
+  ],
+  {
+    stdio: "inherit",
+  }
 );
