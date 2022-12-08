b0dd2fb6547c7fb80ced6e5dfa231ec1d5d1dbeb
Tristan Timblin <ttimblin@yext.com>
2022-12-08 11:52:55 -0500

    cleanup with afterEach

--- packages/pages/src/util/runtime.test.ts
+++ packages/pages/src/util/runtime.test.ts
@@ -7,7 +7,7 @@ import { getRuntime } from "./runtime.js";
 jest.mock("browser-or-node", () => ({
   __esModule: true,
   isNode: false,
-  isDeno: true,
+  isDeno: false,
   isBrowser: true,
 }));
 
@@ -22,99 +22,90 @@ declare global {
     | undefined;
 }
 
+const originalProcess = process;
+
 describe("runtime", () => {
+  afterEach(() => {
+    (browserOrNode.isDeno as any) = false;
+    (browserOrNode.isNode as any) = false;
+    global.process = originalProcess;
+    global.Deno = undefined;
+  });
+
   it("correctly identifies node and it's major version", async () => {
-    const originalProcess = setupMockEnvironment("node");
+    setupMockEnvironment("node");
 
     const runtime = getRuntime();
 
     expect(runtime.name).toEqual("node");
     expect(runtime.version).toEqual("16.16.0");
     expect(runtime.getNodeMajorVersion()).toEqual(16);
-
-    global.process = originalProcess;
   });
 
   it("returns true for isServerSide when the runtime is node.", async () => {
-    const originalProcess = setupMockEnvironment("node");
+    setupMockEnvironment("node");
 
     const runtime = getRuntime();
 
     expect(runtime.isServerSide).toBe(true);
-
-    global.process = originalProcess;
   });
 
   it("correctly identifies deno via Deno object", async () => {
-    const originalProcess = setupMockEnvironment("deno");
+    setupMockEnvironment("deno");
 
     const runtime = getRuntime();
 
     expect(runtime.name).toEqual("deno");
     expect(runtime.version).toEqual("1.24.0");
-
-    global.process = originalProcess;
-    global.Deno = undefined;
   });
 
   it("returns true for isServerSide when runtime is deno", async () => {
-    const originalProcess = setupMockEnvironment("deno");
+    setupMockEnvironment("deno");
 
     const runtime = getRuntime();
     expect(runtime.isServerSide).toBe(true);
-
-    global.process = originalProcess;
-    global.Deno = undefined;
   });
 
   it("getNodeMajorVersion() throws when not node", async () => {
-    const originalProcess = setupMockEnvironment("deno");
+    setupMockEnvironment("deno");
 
     const runtime = getRuntime();
 
     expect(() => runtime.getNodeMajorVersion()).toThrow();
-
-    global.process = originalProcess;
   });
 
   it("correctly identifies browser", async () => {
-    const originalProcess = setupMockEnvironment();
+    setupMockEnvironment();
 
     const runtime = getRuntime();
 
     expect(runtime.name).toEqual("browser");
-
-    global.process = originalProcess;
   });
 
   it("returns false for isServerSide when runtime is browser", async () => {
-    const originalProcess = setupMockEnvironment();
+    setupMockEnvironment();
 
     const runtime = getRuntime();
 
     expect(runtime.isServerSide).toBe(false);
-
-    global.process = originalProcess;
   });
 });
 
-const setupMockEnvironment = (
-  simulatedEnvironment?: "node" | "deno"
-): NodeJS.Process => {
+const setupMockEnvironment = (simulatedEnvironment?: "node" | "deno") => {
   const originalProcess = process;
   (global.process as any) = undefined;
 
   if (simulatedEnvironment === "deno") {
+    (browserOrNode.isDeno as any) = true;
     global.Deno = {
       version: {
         deno: "1.24.0",
       },
     };
-    (browserOrNode.isDeno as any) = true;
-    (browserOrNode.isNode as any) = false;
   }
 
   if (simulatedEnvironment === "node") {
+    (browserOrNode.isNode as any) = true;
     global.process = {
       ...originalProcess,
       versions: {
@@ -128,14 +119,5 @@ const setupMockEnvironment = (
         openssl: "foo",
       },
     };
-    (browserOrNode.isDeno as any) = false;
-    (browserOrNode.isNode as any) = true;
   }
-
-  if (!simulatedEnvironment) {
-    (browserOrNode.isDeno as any) = false;
-    (browserOrNode.isNode as any) = false;
-  }
-
-  return originalProcess;
 };
