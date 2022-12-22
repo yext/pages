e2bd1a23c056e3f8c315245fae8aabb298d0248c
Tristan Timblin <ttimblin@yext.com>
2022-12-22 15:29:02 -0500

    chore(dev/plugin): add file extension to node_module/file import
    file extensions are required for relative imports but not for node_modules. react-dom/server is unique because it is a node_modules import, but it references a specific file. This is fine in Node 16-18, but errors in Node 19. This item fixes support for Node 19
    
    J=SLAP-2501
    TEST=manual
    
    successfully served the site using Node 16, 17, 18, and 19

--- packages/pages/src/vite-plugin/build/buildStart/rendering/templateUtils.ts
+++ packages/pages/src/vite-plugin/build/buildStart/rendering/templateUtils.ts
@@ -1,5 +1,5 @@
 import { createElement } from "react";
-import { renderToString } from "react-dom/server";
+import { renderToString } from "react-dom/server.js";
 import {
   TemplateProps,
   TemplateRenderProps,
