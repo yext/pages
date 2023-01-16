9dd5c1df4727e54c120d9f1de4284dfd20948e68
Tristan Timblin <ttimblin@yext.com>
2023-01-16 09:47:28 -0500

    log

--- packages/pages/src/vite-plugin/build/buildStart/rendering/wrapper.ts
+++ packages/pages/src/vite-plugin/build/buildStart/rendering/wrapper.ts
@@ -35,6 +35,10 @@ export const reactWrapper = <T extends TemplateRenderProps>(
   return `<!DOCTYPE html>
     <html lang=${lang}>
     <head>
+        <meta name="relativePrefixToRoot" content="${
+          props.relativePrefixToRoot
+        }"/>
+        <meta name="BASE_URL" content="${import.meta.env.BASE_URL}"/>
         <script>window.__INITIAL__DATA__ = ${JSON.stringify(props)}</script>
         ${Array.from(
           getCssTags(
@@ -44,7 +48,7 @@ export const reactWrapper = <T extends TemplateRenderProps>(
           )
         )
           .map((f) => {
-            const cssUrl = new URL(f, import.meta.url);
+            const cssUrl = new URL(f, import.meta.env.BASE_URL);
             return `<link rel="stylesheet" href="${cssUrl}"/>`;
           })
           .join("\n")}
