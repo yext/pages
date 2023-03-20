fcf8d280f3f699f6532070cd89c08de94956620e
Tom Meyer <tmeyer@yext.com>
2023-03-20 14:24:57 -0400

    Update generation.

--- packages/pages/src/vite-plugin/build/closeBundle/functionMetadata.ts
+++ packages/pages/src/vite-plugin/build/closeBundle/functionMetadata.ts
@@ -1,21 +1,21 @@
-import fs from "fs";
-import path from "path";
-import esbuild from "esbuild";
-import { importFromString } from "module-from-string";
-import glob from "glob";
+import fs from "fs"
+import path from "path"
+import esbuild from "esbuild"
+import { importFromString } from "module-from-string"
+import glob from "glob"
 
-const FUNCTIONS_PATH = path.resolve("functions");
+const FUNCTIONS_PATH = path.resolve("functions")
 const FUNCTION_METADATA_PATH = path.join(
   FUNCTIONS_PATH,
   "functionMetadata.json"
-);
-const TEMP_DIR = ".temp";
+)
+const TEMP_DIR = ".temp"
 
 /** Metadata for a serverless function. */
 type FunctionMetadata = {
   /** Name of the function. */
-  entrypoint: string;
-};
+  entrypoint: string
+}
 
 /**
  * Returns a mapping of file path (relative to the repo root) to the metadata
@@ -26,38 +26,55 @@ const getFunctionMetadataMap = async (): Promise<
 > => {
   const filepaths = glob
     .sync(path.join(FUNCTIONS_PATH, "**/*.{js,ts}"), { nodir: true })
-    .map((f) => path.resolve(f));
-  const functionMetadataArray: [string, FunctionMetadata][] = await Promise.all(
-    filepaths.map(async (filepath) => {
-      const buildResult = await esbuild.build({
-        entryPoints: [filepath],
-        outdir: TEMP_DIR,
-        write: false,
-        format: "esm",
-        bundle: true,
-      });
-      const importedFile = await importFromString(
-        buildResult.outputFiles[0].text
-      );
-      const relativePath = path.relative(process.cwd(), filepath);
-      return [relativePath, { entrypoint: importedFile.default?.name }];
-    })
-  );
+    .map(f => path.resolve(f))
+  const functionMetadataArray: [string, FunctionMetadata][] = []
+
+  await Promise.allSettled(filepaths.map(generateFunctionMetadata)).then(
+    results =>
+      results.forEach(result => {
+        if (result.status === "fulfilled") {
+          functionMetadataArray.push(result.value)
+        } else {
+          console.error(result.reason)
+        }
+      })
+  )
+
   return Object.fromEntries(
     functionMetadataArray.filter(([, metadata]) => metadata.entrypoint)
-  );
-};
+  )
+}
+
+async function generateFunctionMetadata(
+  filepath: string
+): Promise<[string, FunctionMetadata]> {
+  const buildResult = await esbuild.build({
+    entryPoints: [filepath],
+    outdir: TEMP_DIR,
+    write: false,
+    format: "esm",
+    bundle: true,
+  })
+  const importedFile = await importFromString(buildResult.outputFiles[0].text)
+
+  if (!importedFile.default) {
+    return Promise.reject("Blah")
+  }
+
+  const relativePath = path.relative(process.cwd(), filepath)
+  return [relativePath, { entrypoint: importedFile.default?.name }]
+}
 
 /** Generates a functionMetadata.json file from the functions directory. */
 export const generateFunctionMetadataFile = async () => {
-  const functionMetadataMap = await getFunctionMetadataMap();
+  const functionMetadataMap = await getFunctionMetadataMap()
   fs.writeFileSync(
     FUNCTION_METADATA_PATH,
     JSON.stringify(functionMetadataMap, null, 2)
-  );
-};
+  )
+}
 
 /** Returns whether or not a functionMetadata.json file should be generated. */
 export const shouldGenerateFunctionMetadata = () => {
-  return fs.existsSync(FUNCTIONS_PATH);
-};
+  return fs.existsSync(FUNCTIONS_PATH)
+}
