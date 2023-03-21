578ee949cb2acb69d41eb1aa5bf9b8aa11913620
Tom Meyer <tmeyer@yext.com>
2023-03-21 09:49:58 -0400

    feat(plugin): update `functionMetadata` generation logic (#295)
    Updated the generation logic for the `functionMetadata.json` file. Now, if there's a file without a `default` export, we log an error in the console that includes the file name. 
    
    J=SLAP-2658
    TEST=manual

--- packages/pages/src/vite-plugin/build/closeBundle/functionMetadata.ts
+++ packages/pages/src/vite-plugin/build/closeBundle/functionMetadata.ts
@@ -3,13 +3,15 @@ import path from "path";
 import esbuild from "esbuild";
 import { importFromString } from "module-from-string";
 import glob from "glob";
+import chalk from "chalk";
+import os from "os";
 
 const FUNCTIONS_PATH = path.resolve("functions");
 const FUNCTION_METADATA_PATH = path.join(
   FUNCTIONS_PATH,
   "functionMetadata.json"
 );
-const TEMP_DIR = ".temp";
+const TEMP_DIR = os.tmpdir();
 
 /** Metadata for a serverless function. */
 type FunctionMetadata = {
@@ -19,7 +21,9 @@ type FunctionMetadata = {
 
 /**
  * Returns a mapping of file path (relative to the repo root) to the metadata
- * for the function that is the default export of that file.
+ * for the function that is the default export of that file. If there is no default
+ * export in the file, it will be left out of the mapping and an error will be logged
+ * in the console.
  */
 const getFunctionMetadataMap = async (): Promise<
   Record<string, FunctionMetadata>
@@ -27,26 +31,50 @@ const getFunctionMetadataMap = async (): Promise<
   const filepaths = glob
     .sync(path.join(FUNCTIONS_PATH, "**/*.{js,ts}"), { nodir: true })
     .map((f) => path.resolve(f));
-  const functionMetadataArray: [string, FunctionMetadata][] = await Promise.all(
-    filepaths.map(async (filepath) => {
-      const buildResult = await esbuild.build({
-        entryPoints: [filepath],
-        outdir: TEMP_DIR,
-        write: false,
-        format: "esm",
-        bundle: true,
-      });
-      const importedFile = await importFromString(
-        buildResult.outputFiles[0].text
-      );
-      const relativePath = path.relative(process.cwd(), filepath);
-      return [relativePath, { entrypoint: importedFile.default?.name }];
+
+  const results = await Promise.allSettled(
+    filepaths.map(generateFunctionMetadata)
+  );
+  const functionMetadataArray: [string, FunctionMetadata][] = results
+    .filter((result) => {
+      if (result.status === "fulfilled") {
+        return true;
+      } else {
+        console.error(chalk.red(result.reason));
+        return false;
+      }
     })
-  );
-  return Object.fromEntries(
-    functionMetadataArray.filter(([, metadata]) => metadata.entrypoint)
-  );
+    .map((result) => {
+      return (result as PromiseFulfilledResult<[string, FunctionMetadata]>)
+        .value;
+    });
+
+  return Object.fromEntries(functionMetadataArray);
 };
+
+/**
+ * Generates a tuple containing the relative path of the file and its {@link FunctionMetadata}.
+ * If the file does not contain a default export, a rejected {@link Promise} will be returned.
+ */
+async function generateFunctionMetadata(
+  filepath: string
+): Promise<[string, FunctionMetadata]> {
+  const buildResult = await esbuild.build({
+    entryPoints: [filepath],
+    outdir: TEMP_DIR,
+    write: false,
+    format: "esm",
+    bundle: true,
+  });
+  const importedFile = await importFromString(buildResult.outputFiles[0].text);
+  const relativePath = path.relative(process.cwd(), filepath);
+
+  if (!importedFile.default) {
+    throw new Error(`${relativePath} does not contain a default export.`);
+  }
+
+  return [relativePath, { entrypoint: importedFile.default.name }];
+}
 
 /** Generates a functionMetadata.json file from the functions directory. */
 export const generateFunctionMetadataFile = async () => {
