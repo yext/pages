f2859eb5628a0e8a9578b8f39325107325b2546e
Oliver Shi <shishishi123@gmail.com>
2023-04-21 10:43:39 -0400

    test(dev): playwright tests for static-site (#300)
    Takes https://github.com/yext/pages/pull/224, simplifies the setup, and
    removes tests for entity pages.
    
    I cannot run tests in safari locally, since my macos version is too old
    (and I'm unable to upgrade it due to IT restrictions).
    At first, I wanted to only run chrome tests locally and only add safari
    and firefox in CI. However, in order to check-in
    snapshots I have to run the tests locally.
    Probably there should be a way to upload snapshots from CI, since we'll
    want to have snapshots in additional environments
    (windows + linux) that we don't have access to locally. Once that
    infrastructure is in place, we can remove the acceptance_test.yml we
    have for ensuring the pages dev server can start in windows and linux.
    
    I don't think the current prepare script in PagesJS is ideal for fresh
    clone scenarios.
    The first pnpm install will run the build for PagesJS, however because
    the build occurs AFTER the install,
    on a fresh clone of the repo, the built assets will not be linked
    throughout the monorepo, so another pnpm install
    is needed, which will also do an extra build of PagesJS. Running the
    build an extra time isn't ideal.
    
    J=none
    TEST=auto
    
    ---------
    
    Co-authored-by: Duval Kilpatrick <mkilpatrick@yext.com>
    Co-authored-by: nmanu1 <nmanu@yext.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ playground/playwright.config.ts
@@ -0,0 +1,52 @@
+import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from "@playwright/test";
+
+const base: PlaywrightTestConfig = {
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000,
+    toHaveScreenshot: {
+      threshold: 0.01,
+      maxDiffPixelRatio: 0.01,
+    },
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: "html",
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: "http://localhost:5173",
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: "on-first-retry",
+    video: "retain-on-failure",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
+    {
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+      },
+    },
+  ],
+};
+
+export default base;
