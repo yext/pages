128f92716460f7be02eb468b610946db003ae4f2
nmanu1 <nmanu@yext.com>
2023-04-21 13:43:13 -0400

    test(playground): add multibrand playwright tests

---
+++ playground/multibrand-site/__tests__/dev.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test";
+
+const waitForStyling = async () => {
+  await new Promise((r) => setTimeout(r, 500));
+};
+
+test("index page loads", async ({ page }) => {
+  await page.goto("/");
+  await waitForStyling();
+  await expect(page).toHaveTitle(/Pages Development Page/);
+  await expect(page).toHaveScreenshot();
+});
+
+test("robots page loads", async ({ page }) => {
+  await page.goto("/robots.txt");
+  await waitForStyling();
+  expect(page.locator("pre")).toHaveText("robots");
+  await expect(page).toHaveScreenshot();
+});
+
+test("static page loads", async ({ page }) => {
+  await page.goto("/index.html");
+  await waitForStyling();
+  await expect(page.locator("body > div")).toHaveText("Static Page");
+  await expect(page).toHaveScreenshot();
+});
+
+test("entity page loads", async ({ page }) => {
+  await page.goto("/sunglasses/2272132526004354756");
+  await waitForStyling();
+  await expect(page.locator("body > div")).toHaveText("Holbrook");
+  await expect(page).toHaveScreenshot();
+});
