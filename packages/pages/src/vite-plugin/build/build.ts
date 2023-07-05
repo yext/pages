c0380ecb4c86303ad0eb9a5c73ce82ec1394242a
Duval Kilpatrick <mkilpatrick@yext.com>
2023-07-05 17:00:53 -0400

    fix entryFile names

--- packages/pages/src/vite-plugin/build/build.ts
+++ packages/pages/src/vite-plugin/build/build.ts
@@ -42,6 +42,7 @@ export const build = (projectStructure: ProjectStructure): Plugin => {
               intro,
               assetFileNames: "assets/static/[name]-[hash][extname]",
               chunkFileNames: "assets/static/[name]-[hash].js",
+              entryFileNames: "assets/[name].[hash].js",
             },
           },
           reportCompressedSize: false,
