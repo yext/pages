0dba61fb21d140eddb17bda86e2b06471443f910
Jacob Wartofsky <jwartofsky@yext.com>
2023-07-13 19:30:35 -0400

    chore: migrate from Yargs to CommanderJS
    Ran Playwright tests, 'pnpm run test', and ran build:serve on
    starter repo without issue.

--- packages/pages/src/generate/features/features.ts
+++ packages/pages/src/generate/features/features.ts
@@ -1,52 +1,44 @@
 import path from "path";
-import { CommandModule } from "yargs";
 import { Path } from "../../common/src/project/path.js";
-import {
-  ProjectFilepaths,
-  ProjectStructure,
-} from "../../common/src/project/structure.js";
+import { ProjectStructure } from "../../common/src/project/structure.js";
 import { getTemplateFilepaths } from "../../common/src/template/internal/getTemplateFilepaths.js";
 import { loadTemplateModules } from "../../common/src/template/internal/loader.js";
 import { createFeaturesJson } from "./createFeaturesJson.js";
+import { Command } from "commander";
 
-type FeaturesArgs = Pick<ProjectFilepaths, "scope">;
-
-const handler = async ({ scope }: FeaturesArgs): Promise<void> => {
-  const projectStructure = new ProjectStructure();
-  const templatesRoot = projectStructure.templatesRoot.path;
-  const sitesConfigRoot = projectStructure.sitesConfigRoot.path;
-  const templateRootAbsolutePath = path.join(process.cwd(), templatesRoot);
-  const templateFilepaths = getTemplateFilepaths(
-    scope
-      ? [
-          new Path(path.join(templateRootAbsolutePath, scope)),
-          new Path(templateRootAbsolutePath),
-        ]
-      : [new Path(templateRootAbsolutePath)]
-  );
-  const templateModules = await loadTemplateModules(
-    templateFilepaths,
-    true,
-    false
-  );
-  const featuresFilepath = path.join(
-    process.cwd(),
-    sitesConfigRoot,
-    scope ?? "",
-    "features.json"
-  );
-  await createFeaturesJson(templateModules, featuresFilepath);
-};
-
-export const featureCommandModule: CommandModule<unknown, FeaturesArgs> = {
-  command: "features",
-  describe: "Generates features.json file",
-  builder: (yargs) => {
-    return yargs.option("scope", {
-      describe: "The subfolder to scope the served templates from",
-      type: "string",
-      demandOption: false,
+export const featureCommand = (program: Command) => {
+  program
+    .command("features")
+    .description("Generates features.json file")
+    .option(
+      "--scope <string>",
+      "The subfolder to scope the served templates from"
+    )
+    .action(async (options) => {
+      const scope = options.scope;
+      const projectStructure = new ProjectStructure();
+      const templatesRoot = projectStructure.templatesRoot.path;
+      const sitesConfigRoot = projectStructure.sitesConfigRoot.path;
+      const templateRootAbsolutePath = path.join(process.cwd(), templatesRoot);
+      const templateFilepaths = getTemplateFilepaths(
+        scope
+          ? [
+              new Path(path.join(templateRootAbsolutePath, scope)),
+              new Path(templateRootAbsolutePath),
+            ]
+          : [new Path(templateRootAbsolutePath)]
+      );
+      const templateModules = await loadTemplateModules(
+        templateFilepaths,
+        true,
+        false
+      );
+      const featuresFilepath = path.join(
+        process.cwd(),
+        sitesConfigRoot,
+        scope ?? "",
+        "features.json"
+      );
+      await createFeaturesJson(templateModules, featuresFilepath);
     });
-  },
-  handler,
 };
--- packages/pages/src/generate/generate.ts
+++ packages/pages/src/generate/generate.ts
@@ -1,17 +1,14 @@
-import { CommandModule } from "yargs";
-import { featureCommandModule } from "./features/features.js";
-import { ciCommandModule } from "./ci/ci.js";
+import { Command } from "commander";
+import { featureCommand } from "./features/features.js";
+import { ciCommand } from "./ci/ci.js";
 
-export const generateCommandModule: CommandModule = {
-  command: "generate",
-  describe: "Generates a specific file",
-  builder: (yargs) => {
-    return yargs
-      .command(featureCommandModule)
-      .command(ciCommandModule)
-      .demandCommand();
-  },
-  handler: () => {
-    console.log('Must provide a subcommand of "generate".');
-  },
+export const generateCommand = (program: Command) => {
+  const generate = program
+    .command("generate")
+    .description("Generates a specific file")
+    .action(() => {
+      console.log('Must provide a subcommand of "generate".');
+    });
+  featureCommand(generate);
+  ciCommand(generate);
 };
