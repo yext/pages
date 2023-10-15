fab1bda4f83eec9d99cadc3cc5714783517801de
Jacob Wartofsky <jwartofsky@yext.com>
2023-10-15 19:03:32 -0400

    added some logging stuff

--- packages/pages/scripts/install_and_test.js
+++ packages/pages/scripts/install_and_test.js
@@ -1,1 +1,10 @@
-console.log("Hello world!");
+import path from "path";
+import fs from "fs";
+
+const latestPath = path.resolve("latest");
+const packagePath = path.resolve(
+  latestPath,
+  fs.readdirSync(latestPath.toString())[0]
+);
+
+console.log(packagePath);
