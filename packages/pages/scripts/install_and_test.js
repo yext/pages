c67301fc5e8f884764ebf2f9d5094749b0f1e25e
Jacob Wartofsky <jwartofsky@yext.com>
2023-10-15 20:28:08 -0400

    added more logs again

--- packages/pages/scripts/install_and_test.js
+++ packages/pages/scripts/install_and_test.js
@@ -17,7 +17,7 @@ sdks.forEach((sdk) => {
     cwd: `../../jstest/sdks/${sdk}`,
   };
   console.log(`npm install @yext/pages ${packagePath}`);
-  child_process.execSync("npm -v && node -v");
+  console.log(child_process.execSync("npm -v && node -v"));
   child_process.execSync(`npm ci`, cwd);
   child_process.execSync(`npm install @yext/pages ${packagePath}`, cwd);
   child_process.execSync("npm run playwright", cwd);
