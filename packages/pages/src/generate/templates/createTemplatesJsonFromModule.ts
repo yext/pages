72302ae230493fde00d062cdbe882d851204cc36
briantstephan <70166284+briantstephan@users.noreply.github.com>
2023-10-17 11:42:08 -0400

    fix(dev): prevent duplicate stream definitions (#408)
    This prevents shared/duplicate stream definitions from appearing
    multiple times in features.json. If conflicting stream definitions exist
    for the same stream ID, an error is thrown. Verified that duplicate
    identical stream definitions are ignored and duplicate but differing
    stream definitions cause an error.
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/pages/src/generate/templates/createTemplatesJsonFromModule.ts
+++ packages/pages/src/generate/templates/createTemplatesJsonFromModule.ts
@@ -8,6 +8,10 @@ import {
 import { ProjectStructure } from "../../common/src/project/structure.js";
 import { StreamConfig } from "../../common/src/feature/stream.js";
 import { TemplateModuleCollection } from "../../common/src/template/loader/loader.js";
+import {
+  mergeFeatureJson,
+  pushStreamConfigIfValid,
+} from "./createTemplatesJson.js";
 
 // TODO: rename functions in this file once there is a migration flow and checks for it
 // TODO: mergeFeatureJson will no longer be necessary
@@ -22,7 +26,7 @@ export const getFeaturesConfig = (
     const featureConfig = convertTemplateConfigToFeatureConfig(module.config);
     features.push(featureConfig);
     module.config.stream &&
-      streams.push({
+      pushStreamConfigIfValid(streams, {
         ...module.config.stream,
         source: "knowledgeGraph",
         destination: "pages",
@@ -58,25 +62,4 @@ export const createFeaturesJson = (
     featuresAbsolutePath,
     JSON.stringify(featuresJson, null, "  ")
   );
-};
-
-/**
- * Overwrites the "features" and "streams" fields in the feature.json while keeping other fields
- * if the feature.json already exists.
- */
-export const mergeFeatureJson = (
-  featurePath: string,
-  features: FeatureConfig[],
-  streams: any
-): FeaturesConfig => {
-  let originalFeaturesJson = {} as any;
-  if (fs.existsSync(featurePath)) {
-    originalFeaturesJson = JSON.parse(fs.readFileSync(featurePath).toString());
-  }
-
-  return {
-    ...originalFeaturesJson,
-    features,
-    streams,
-  };
 };
