2542e04d406ba6c75bb61cbd75a8c8d0545e3bcc
Anh Thu Nguyen <anguyen@yext.com>
2023-11-21 13:30:41 -0500

    fix(plugin): remove old artifactStructure assets

--- packages/pages/src/generate/artifacts/createArtifactsJson.ts
+++ packages/pages/src/generate/artifacts/createArtifactsJson.ts
@@ -45,11 +45,6 @@ export const getArtifactsConfig = async (
         },
         // static assets based on the Vite publicDir
         {
-          root: projectStructure.config.rootFolders.dist,
-          pattern: "*",
-        },
-        // public assets from the plugin
-        {
           root: `${projectStructure.config.rootFolders.dist}/public_assets`,
           pattern: "**/*",
         },
