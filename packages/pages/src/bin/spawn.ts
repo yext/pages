d5c44149a1fb013772f188402392a5323dcff5ad
Alexis Sanehisa <asanehisa@yext.com>
2024-03-19 15:24:12 -0400

    more logs

--- packages/pages/src/bin/spawn.ts
+++ packages/pages/src/bin/spawn.ts
@@ -4,7 +4,9 @@ import { fileURLToPath, pathToFileURL } from "node:url";
 import process from "process";
 import path from "path";
 import { register } from "node:module";
+console.log("before fileURLToPath");
 const __filename = fileURLToPath(import.meta.url);
+console.log("after fileURLToPath");
 const __dirname = path.dirname(__filename);
 
 const filePrefix = path.sep === path.win32.sep ? "file:\\\\" : "";
@@ -21,6 +23,7 @@ const experimentalFlags = ["--experimental-vm-modules"];
 if (nodeVersion === 18) {
   experimentalFlags.push("--experimental-specifier-resolution=node");
 } else {
+  console.log("pathToLoader " + pathToLoader);
   const loaderURL = pathToFileURL(pathToLoader).href;
   console.log("test (to remove): " + loaderURL.toString());
   register(loaderURL);
