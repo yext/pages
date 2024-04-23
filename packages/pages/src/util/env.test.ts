fc3a80ecd73ac6818c13d2850ac7ad503ef1cc34
Matt Kilpatrick <mkilpatrick@yext.com>
2024-04-23 13:28:23 -0400

    feat(dev/plugin): update isProduction logic (#521)
    Instead of requiring a list of prod domains to check against,
    isProduction now uses the IS_PRODUCTION global variable which is
    guaranteed to be correct as it's injected by the serving layer.

--- packages/pages/src/util/env.test.ts
+++ packages/pages/src/util/env.test.ts
@@ -1,162 +1,53 @@
 // @vitest-environment jsdom
 import { describe, it, expect, vi } from "vitest";
 import { isProduction } from "./env.js";
-import * as runTime from "./runtime.js";
 
 describe("isProduction", () => {
-  it("returns true when browser and prod domain", async () => {
-    const runtimeSpy = vi.spyOn(runTime, "getRuntime") as unknown as any;
-    runtimeSpy.mockImplementation(() => ({
-      name: "browser",
-    }));
-
-    const domain = "prod.com";
-
+  it("returns true when IS_PRODUCTION is true", async () => {
     const windowSpy = vi.spyOn(window, "window", "get") as unknown as any;
     windowSpy.mockImplementation(() => ({
-      location: {
-        hostname: "prod.com",
-      },
-    }));
-
-    expect(isProduction(domain)).toBeTruthy();
-
-    windowSpy.mockRestore();
-    runtimeSpy.mockRestore();
-  });
-
-  it("returns false when not browser and prod domain", async () => {
-    const runtimeSpy = vi.spyOn(runTime, "getRuntime") as unknown as any;
-    runtimeSpy.mockImplementation(() => ({
-      name: "node",
-    }));
-
-    const domain = "prod.com";
-
-    const windowSpy = vi.spyOn(window, "window", "get") as unknown as any;
-    windowSpy.mockImplementation(() => ({
-      location: {
-        hostname: "prod.com",
-      },
-    }));
-
-    expect(isProduction(domain)).toBeFalsy();
-
-    windowSpy.mockRestore();
-    runtimeSpy.mockRestore();
-  });
-
-  it("returns false when browser and staging domain", async () => {
-    const runtimeSpy = vi.spyOn(runTime, "getRuntime") as unknown as any;
-    runtimeSpy.mockImplementation(() => ({
-      name: "browser",
-    }));
-
-    const windowSpy = vi.spyOn(window, "window", "get") as unknown as any;
-    windowSpy.mockImplementation(() => ({
-      location: {
-        hostname: "staging.com",
-      },
-    }));
-
-    expect(isProduction("prod.com")).toBeFalsy();
-
-    windowSpy.mockRestore();
-    runtimeSpy.mockRestore();
-  });
-
-  it("returns false when browser and staging domain and multiple allowed prod domains", async () => {
-    const runtimeSpy = vi.spyOn(runTime, "getRuntime") as unknown as any;
-    runtimeSpy.mockImplementation(() => ({
-      name: "browser",
-    }));
-
-    const windowSpy = vi.spyOn(window, "window", "get") as unknown as any;
-    windowSpy.mockImplementation(() => ({
-      location: {
-        hostname: "staging.com",
-      },
-    }));
-
-    expect(isProduction("prod1.com", "prod2.com")).toBeFalsy();
-
-    windowSpy.mockRestore();
-    runtimeSpy.mockRestore();
-  });
-
-  it("returns true when browser and prod domain and multiple allowed prod domains", async () => {
-    const runtimeSpy = vi.spyOn(runTime, "getRuntime") as unknown as any;
-    runtimeSpy.mockImplementation(() => ({
-      name: "browser",
-    }));
-
-    const windowSpy = vi.spyOn(window, "window", "get") as unknown as any;
-    windowSpy.mockImplementation(() => ({
-      location: {
-        hostname: "prod1.com",
-      },
-    }));
-
-    expect(isProduction("prod1.com", "prod2.com")).toBeTruthy();
-
-    windowSpy.mockRestore();
-    runtimeSpy.mockRestore();
-  });
-
-  it("returns true when browser and prod domain and no prod domains specified", async () => {
-    const runtimeSpy = vi.spyOn(runTime, "getRuntime") as unknown as any;
-    runtimeSpy.mockImplementation(() => ({
-      name: "browser",
-    }));
-
-    const windowSpy = vi.spyOn(window, "window", "get") as unknown as any;
-    windowSpy.mockImplementation(() => ({
-      location: {
-        hostname: "prod.com",
-      },
+      IS_PRODUCTION: true,
     }));
 
     expect(isProduction()).toBeTruthy();
 
     windowSpy.mockRestore();
-    runtimeSpy.mockRestore();
   });
 
-  it("returns false when browser and localhost and no prod domains specified", async () => {
-    const runtimeSpy = vi.spyOn(runTime, "getRuntime") as unknown as any;
-    runtimeSpy.mockImplementation(() => ({
-      name: "browser",
-    }));
-
+  it("returns true when IS_PRODUCTION is true and domains passed", async () => {
     const windowSpy = vi.spyOn(window, "window", "get") as unknown as any;
     windowSpy.mockImplementation(() => ({
-      location: {
-        hostname: "localhost",
-      },
+      IS_PRODUCTION: true,
+    }));
+
+    expect(isProduction("random")).toBeTruthy();
+
+    windowSpy.mockRestore();
+  });
+
+  it("returns false when IS_PRODUCTION is false", async () => {
+    const windowSpy = vi.spyOn(window, "window", "get") as unknown as any;
+    windowSpy.mockImplementation(() => ({
+      IS_PRODUCTION: false,
     }));
 
     expect(isProduction()).toBeFalsy();
 
     windowSpy.mockRestore();
-    runtimeSpy.mockRestore();
   });
 
-  it("returns false when browser and preview domain and no prod domains specified", async () => {
-    const runtimeSpy = vi.spyOn(runTime, "getRuntime") as unknown as any;
-    runtimeSpy.mockImplementation(() => ({
-      name: "browser",
-    }));
-
+  it("returns false when IS_PRODUCTION is false and domains passed", async () => {
     const windowSpy = vi.spyOn(window, "window", "get") as unknown as any;
     windowSpy.mockImplementation(() => ({
-      location: {
-        hostname: "test.preview.pagescdn.com",
-      },
+      IS_PRODUCTION: false,
     }));
 
-    expect(isProduction()).toBeFalsy();
+    expect(isProduction("random")).toBeFalsy();
 
     windowSpy.mockRestore();
-    runtimeSpy.mockRestore();
+  });
+
+  it("returns false when IS_PRODUCTION is not defined", async () => {
+    expect(isProduction()).toBeFalsy();
   });
 });
