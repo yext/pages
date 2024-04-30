fcb26479a88a98cec2d097b4a15078fb9edb4761
Jacob <115099870+jwartofsky-yext@users.noreply.github.com>
2024-04-30 12:04:11 -0400

    fix(plugin): export redirect types (#522)

--- packages/pages/src/index.ts
+++ packages/pages/src/index.ts
@@ -3,5 +3,6 @@ export { createServer as createDevServer } from "./dev/server/server.js";
 export * from "./common/src/template/head.js";
 export * from "./common/src/template/paths.js";
 export * from "./common/src/template/types.js";
+export * from "./common/src/redirect/types.js";
 export * from "./common/src/function/types.js";
 export * from "./common/src/module/types.js";
