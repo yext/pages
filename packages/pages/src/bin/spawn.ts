1a42bec5d2aa1f5dd81f84e2fa1795f87462e1fd
Alexis Sanehisa <asanehisa@yext.com>
2024-08-07 13:03:58 -0400

    forgot to commit

--- packages/pages/src/bin/spawn.ts
+++ packages/pages/src/bin/spawn.ts
@@ -6,27 +6,11 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const filePrefix = path.sep === path.win32.sep ? "file:\\\\" : "";
 const pathToPagesScript = path.resolve(__dirname, "./pages.js");
-const pathToLoader = filePrefix + path.resolve(__dirname, "./loader.js");
-
-const nodeVersion = Number(
-  spawnSync("node", ["-v"], { encoding: "utf-8" })
-    .stdout.substring(1)
-    .split(".")[0]
-);
-
-const experimentalFlags = ["--experimental-vm-modules"];
-if (nodeVersion === 18) {
-  experimentalFlags.push("--experimental-specifier-resolution=node");
-} else {
-  experimentalFlags.push("--experimental-loader");
-  experimentalFlags.push(pathToLoader);
-}
 
 const results = spawnSync(
   "node",
-  [...experimentalFlags, pathToPagesScript, ...process.argv.slice(2)],
+  [pathToPagesScript, ...process.argv.slice(2)],
   {
     stdio: "inherit",
   }
