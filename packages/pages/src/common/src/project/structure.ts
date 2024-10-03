4c74e43f37a1767650dd1739c03d6fd54e04445e
Matt Kilpatrick <mkilpatrick@yext.com>
2024-10-03 12:23:30 -0400

    fix: use scope when finding vite.config.js (#546)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/pages/src/common/src/project/structure.ts
+++ packages/pages/src/common/src/project/structure.ts
@@ -214,17 +214,22 @@ export class ProjectStructure {
   ) => {
     const config = merge(defaultProjectStructureConfig, projectStructureConfig);
 
+    let viteConfigPath = pathLib.resolve(config.scope ?? "", "vite.config.js");
+    if (config.scope && !fs.existsSync(viteConfigPath)) {
+      viteConfigPath = pathLib.resolve("vite.config.js");
+    }
+
     // TODO: handle other extensions
     const assetsDir = await determineAssetsFilepath(
       DEFAULT_ASSETS_DIR,
-      pathLib.resolve("vite.config.js")
+      viteConfigPath
     );
 
     config.subfolders.assets = assetsDir;
 
     const publicDir = await determinePublicFilepath(
       DEFAULT_PUBLIC_DIR,
-      pathLib.resolve("vite.config.js")
+      viteConfigPath
     );
 
     config.subfolders.public = publicDir;
