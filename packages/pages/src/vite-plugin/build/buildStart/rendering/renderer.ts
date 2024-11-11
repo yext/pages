5632353f1161ef385dc5c5bb1273eb812c756535
Ben Life <blife@yext.com>
2024-11-11 13:33:54 -0500

    render using code_template if it exists

--- packages/pages/src/vite-plugin/build/buildStart/rendering/renderer.ts
+++ packages/pages/src/vite-plugin/build/buildStart/rendering/renderer.ts
@@ -30,7 +30,7 @@ export default async (
   manifest: Manifest
 ): Promise<GeneratedPage | GeneratedRedirect> => {
   const projectStructure = new ProjectStructure(manifest.projectStructure);
-  const feature = props.document.__.name;
+  const feature = props.document.__.code_template ?? props.document.__.name;
 
   const template = await readTemplateModules(
     feature,
