d8c3adf0bc6fe7b3e36fba49cc1eca13254a1f38
Alexis Sanehisa <106991746+asanehisa@users.noreply.github.com>
2024-11-25 13:56:40 -0500

    fix: migrate eslint docs per version changes (#556)
    [Bug](https://github.com/eslint/eslint/issues/19134) introduced in
    eslint v9.15.0. Ended up not downgrading, but adding additonal rules to
    eslint docs per Jake's comment
    
    Also followed this [migration
    guide](https://eslint.org/docs/latest/use/configure/migration-guide) so
    eslint would work
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,73 @@
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import _import from "eslint-plugin-import";
+import { fixupPluginRules } from "@eslint/compat";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["**/dist", "packages/pages/tests/fixtures", "playground/**"],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ),
+  {
+    plugins: {
+      react,
+      "@typescript-eslint": typescriptEslint,
+      import: fixupPluginRules(_import),
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        node: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-extra-semi": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "import/extensions": ["error", "ignorePackages"],
+      "react/no-deprecated": "off",
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+        },
+      ],
+    },
+  },
+];
