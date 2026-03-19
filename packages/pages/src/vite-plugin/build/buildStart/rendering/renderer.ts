255cd7c71c3391ecb79651dc2e85a1ffe12292a7
Jacob Wartofsky <jwartofsky@yext.com>
2026-03-19 14:06:15 -0400

    rename templateName to feature and revert changes to error message

--- packages/pages/src/vite-plugin/build/buildStart/rendering/renderer.ts
+++ packages/pages/src/vite-plugin/build/buildStart/rendering/renderer.ts
@@ -28,13 +28,13 @@ export default async (
   manifest: Manifest
 ): Promise<GeneratedPage | GeneratedRedirect> => {
   const projectStructure = new ProjectStructure(manifest.projectStructure);
-  const templateName = getDocumentTemplateName(props.document);
+  const feature = getDocumentTemplateName(props.document);
 
-  if (!templateName) {
+  if (!feature) {
     throw new Error("Could not determine template name from document metadata");
   }
 
-  const template = await readTemplateModules(templateName, manifest, projectStructure);
+  const template = await readTemplateModules(feature, manifest, projectStructure);
   if (template) {
     const pluginRenderTemplates = await getPluginRenderTemplates(manifest, projectStructure);
     return await generateTemplateResponses(
@@ -46,10 +46,10 @@ export default async (
     );
   }
 
-  const redirect = await readRedirectModules(templateName, manifest, projectStructure);
+  const redirect = await readRedirectModules(feature, manifest, projectStructure);
   if (redirect) {
     return await generateRedirectResponses(redirect, props);
   }
 
-  throw new Error(`Could not find path for template ${templateName}`);
+  throw new Error(`Could not find path for feature ${feature}`);
 };
